dc8ce93c73c6062f89eecd5f5926d3692dd6153d
Iván <ivanillera@gmail.com>
2021-06-07 14:38:34 -0300

    Inicial Commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+// Enviroment variables.
+app.set('port', process.env.PORT || 4000);
+
+app.use(morgan('dev'))
+
+module.exports = app;
