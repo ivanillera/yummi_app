659acc672ae86d9028ca7243e4380eda84fdadc5
alvarezem <emmanuel_roberto@hotmail.com>
2021-06-12 23:47:06 -0300

    Cambio de nombre de base de datos

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb+srv://yummiadmin:1234@yummidb.rh4jc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    .connect('mongodb+srv://yummiadmin:1234@yummidb.rh4jc.mongodb.net/yummiDB?retryWrites=true&w=majority',{
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useFindAndModify: false
