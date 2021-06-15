4e09f60b5c9eba9334807b39b5a51d33f4883978
Iván <ivanillera@gmail.com>
2021-06-14 22:16:22 -0300

    Agregado referencias a bases de datos

--- backend/src/models/Subject.js
+++ backend/src/models/Subject.js
@@ -1,10 +1,9 @@
-
 const { Schema, model } = require('mongoose')
 
 const subjectSchema = new Schema({
     name: {type: String, required:true},
     professor: {type: String, required: true},
-    notes: {type: [String], required: true} // Contiene el hilo de apuntes!
+    notes: {type: Schema.ObjectId, ref:'Note', required: false} // Contiene el hilo de apuntes!
 }, {
     timestamps: true,
     versionKey: false
