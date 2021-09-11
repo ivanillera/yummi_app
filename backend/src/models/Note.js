560efdc8f9a940662d22af9d6ddf28a068be70bf
Emmanuel Alvarez <emmanuel_roberto@hotmail.com>
2021-09-11 15:42:37 -0300

    Arreglos en el backend

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -2,8 +2,11 @@ const { Schema, model } = require('mongoose')
 
 const noteSchema = new Schema({
     name: {type: String, required:true},
-    creator: {type: String, required: true},
-    content: {type: String, required: true} // Contenido de apunte: Adjuntar archivo.
+    carrer: {type: String, required:true},
+    creator: {type: Schema.ObjectId, ref:'User', required: true},
+    subject: {type: Schema.ObjectId, ref:'Subject', required: true},
+    content: {type: String, required: true},
+    attached: {type: String, required: true} // Contenido de apunte: Adjuntar archivo.
 },{
     timestamps: true,
     versionKey: false
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,6 +3,8 @@ const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
     name: {type: String, required:true},
+    mail: {type: String, required:true},
+    birth: {type: String, required:true},
     notes: {type: Schema.ObjectId, ref:'Note', required: false} // Contiene el hilo de apuntes creados
 }, {
     timestamps: true,
