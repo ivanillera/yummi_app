1c1798c559f2b7b85fcff9949555be53d18f9b29
Iván <ivanillera@gmail.com>
2021-09-12 17:13:39 -0300

    Agregadas tags

--- frontendyummi/src/app/app-routing.module.ts
+++ frontendyummi/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CrearApunteComponent } from './components/pages/crear-apunte/crear-apunte.component';
+import { PerfilComponent } from './components/pages/perfil/perfil.component';
 import { SearchbarComponent } from './components/pages/searchbar/searchbar.component';
 import { ThreadComponent } from './components/pages/thread/thread.component';
 
@@ -11,6 +13,12 @@ const routes: Routes = [
   {
     path:'apuntes', component:ThreadComponent
   },
+  {
+    path:'perfil', component:PerfilComponent
+  },
+  {
+    path:'crear', component:CrearApunteComponent
+  }
 ];
 
 @NgModule({
