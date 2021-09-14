b8e85e380cba1f34edf9e590db8d52ea68dae477
Iván <ivanillera@gmail.com>
2021-09-13 22:30:18 -0300

    Nueva fuente | Cambios en lista apuntes | Intento implementación de iteración de apuntes.

--- frontendyummi/src/app/components/pages/crear-apunte/crear-apunte.component.ts
+++ frontendyummi/src/app/components/pages/crear-apunte/crear-apunte.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Apunte } from 'src/app/models/Apunte';
 
 @Component({
   selector: 'app-crear-apunte',
@@ -11,5 +12,4 @@ export class CrearApunteComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
