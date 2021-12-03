eb170b2f3e8ed475226e2729cc0dfe9a7d376d98
Ivan <ivanillera@gmail.com>
2021-12-03 20:48:45 -0300

    Cambios en modelaje

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,7 +4,7 @@ const { Schema, model } = require('mongoose')
 const userSchema = new Schema({
     name: {type: String, required:true},
     mail: {type: String, required:true},
-    birth: {type: String, required:true},
+    // birth: {type: String, required:true},
     notes: {type: Schema.ObjectId, ref:'Note', required: false} // Contiene el hilo de apuntes creados
 }, {
     timestamps: true,
