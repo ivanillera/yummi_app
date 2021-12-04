32e13e5a91abf144d09a289e706dffcc89ecc704
Emmanuel Alvarez <emmanuel_roberto@hotmail.com>
2021-12-03 22:15:29 -0300

    Pruebas, cualquier cosa corregir :P

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,19 @@ const cors = require('cors')
 const app = express();
 
 
+//TODO MEJORAR ESTO --------
+
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+
+const mongoURI = 'mongodb+srv://yummiadmin:1234@yummidb.rh4jc.mongodb.net/yummiDB?retryWrites=true&w=majority'
+
+//TODO MEJORAR ESTO --------
+
 app.set('port', process.env.PORT || 4000);
 
 app.use(cors());
@@ -19,3 +32,36 @@ app.use("/api/files", require('./routes/files.routes'));
 app.use("/api/comments", require('./routes/comments.routes'));
 
 module.exports = app;
+
+var conn = mongoose.createConnection(mongoURI);
+    conn.once('open', function () {
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+    })
+    
+    const storage = new GridFsStorage({
+        url: mongoURI,
+        file: (req, file) => {
+          return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+              if (err) {
+                return reject(err);
+              }
+              const filename = buf.toString('hex') + path.extname(file.originalname);
+              const fileInfo = {
+                filename: filename,
+                bucketName: 'uploads'
+              };
+              resolve(fileInfo);
+            });
+          });
+        }
+      });
+
+      const upload = multer({ storage });
+
+//TODO - MEJORAR ESTO
+
+      app.post('/upload', upload.single('attached'), (req, res) => {
+        res.json({file: req.file});
+      })
--- backend/src/database.js
+++ backend/src/database.js
@@ -17,28 +17,4 @@ mongoose
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
     
-    var conn = mongoose.createConnection(mongoURI);
-    conn.once('open', function () {
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('uploads');
-    })
     
-    const storage = new GridFsStorage({
-        url: mongoURI,
-        file: (req, file) => {
-          return new Promise((resolve, reject) => {
-            crypto.randomBytes(16, (err, buf) => {
-              if (err) {
-                return reject(err);
-              }
-              const filename = buf.toString('hex') + path.extname(file.originalname);
-              const fileInfo = {
-                filename: filename,
-                bucketName: 'uploads'
-              };
-              resolve(fileInfo);
-            });
-          });
-        }
-      });
-      const upload = multer({ storage });
