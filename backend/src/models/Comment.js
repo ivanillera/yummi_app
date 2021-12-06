109bedd9fa805a780f2df4f5f8d708ce7b6ab3ef
Ivan <ivanillera@gmail.com>
2021-12-05 22:59:53 -0300

    Sincronización comentarios - backend front

--- backend/src/models/Comment.js
+++ backend/src/models/Comment.js
@@ -1,8 +1,9 @@
 const { Schema, model } = require('mongoose')
 
 const commentSchema = new Schema({
-    creator: {type: Schema.ObjectId, ref:'User', required: true},
+    creator: {type: Schema.ObjectId, ref:'User', required: false},
     content: {type: String, required: true},
+    date: {type:String, required:false}
 },{
     timestamps: true,
     versionKey: false
--- frontendyummi/src/app/components/pages/apunte/apunte.component.ts
+++ frontendyummi/src/app/components/pages/apunte/apunte.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Comment } from 'src/app/models/Comment';
 import { CommentsService } from '../../../services/comments.service';
-
+import { FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-apunte',
@@ -14,18 +14,30 @@ export class ApunteComponent implements OnInit {
   commentContent = ""
   commentDate = "21/10/2021"
 
-  addComment(){
-    // Crear un objeto comentario
-    const comment: Comment = {
-      content: this.commentContent,
-      creator: this.commentCreator,
-      date: this.commentDate
-    }
-    // Agregar objeto comentario al array
-    this.listComments.push(comment);
-    // Reset input
-    this.commentContent = '';
+  // addComment(){
+  //   // Crear un objeto comentario
+  //   const comment: Comment = {
+  //     content: this.commentContent,
+  //     creator: this.commentCreator,
+  //     date: this.commentDate
+  //   }
+  //   // Agregar objeto comentario al array
+  //   this.listComments.push(comment);
+  //   // Reset input
+  //   this.commentContent = '';
 
+  // }
+
+  addComment(form: NgForm){
+    console.log(form.value);
+    this.commentService.createComment(form.value).subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
+  }
+
+  resetForm(form: NgForm){ // No se usa.
+    form.reset();
   }
   
 
